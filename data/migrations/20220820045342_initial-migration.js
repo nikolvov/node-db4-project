db12a1fbfd19796a6ac309402a91341fa4535041
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2022-08-19 22:24:59 -0700

    wi[

---
+++ data/migrations/20220820045342_initial-migration.js
@@ -0,0 +1,37 @@
+
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('recipes', table => {
+        table.increments('recipe_id')
+        table.string('recipe_name', 200).notNullable().unique()
+    })
+    .createTable('ingredients', table => {
+        table.increments('ingredient_id')
+        table.string('ingredient_name', 200).notNullable().unique()
+        table.string('ingredient_unit', 50)
+    })
+    .createTable('steps', table => {
+        table.increments('step_id')
+        table.string('step_text', 200).notNullable()
+        table.integer('step_number').notNullable()
+        table.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('recipe_id')
+            .inTable('recipes')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+    })
+    .createTable('step_ingredients', table => {
+        table.increments('')
+    })
+    
+};
+
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists()
+    .dropTableIfExists()
+    .dropTableIfExists()
+    .dropTableIfExists()
+};
